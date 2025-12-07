db29d5fb322598f95765574ea3c28e2bf575b398
Nurzhalgas <kozhamseitov06@gmail.com>
2025-12-07 21:29:21 +0500

    logger multiwrite removed

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -2,9 +2,6 @@ package logger
 
 import (
 	"fmt"
-	"io"
-	"os"
-	"time"
 
 	"github.com/sirupsen/logrus"
 )
@@ -23,17 +20,7 @@ func (h *PrefixHook) Fire(e *logrus.Entry) error {
 }
 
 func SetupLogger(prefix string) *logrus.Logger {
-	logFile, err := os.OpenFile(".log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666)
-	if err != nil {
-		panic(err)
-	}
-	seperator := fmt.Sprintf("=== Logging started at %s ===\n", time.Now().Format("2006-01-02 15:04:05"))
-	logFile.Write([]byte(seperator))
-
-	multiwriter := io.MultiWriter(os.Stdout, logFile)
-
 	logger := logrus.New()
-	logger.SetOutput(multiwriter)
 	logger.SetFormatter(&logrus.TextFormatter{
 		ForceColors:     true,
 		FullTimestamp:   true,
